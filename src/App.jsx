03e88d0cfad40894ee94b252c7b7968ddb382b34
Abdul hanan <abdulhanan78860@gmail.com>
2023-09-09 19:30:41 +0500

    add all changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Abdul Hanan",
+  title: "Web Developer and Content Creator",
+  email: "abdul.hanan@studentambassadors.com",
+  gitHub: "coder-abdulhanan",
+  instagram: "honey_007.official",
+  linkedIn: "Abdul Hanan",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/Laptoptable.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Desktop with accessories and laptop";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My blog website",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "This is my blog website where I breifly explained my expertise and growth toward technology.",
+    url: "https://coder-abdulhanan.github.io/MlsaTask/",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Introduction to AI",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Artificial intelligence is getting important to know day by day for humans, basics explained in this video.",
+    url: "https://youtu.be/ad79nYk2keg?si=rw_at6E0hqV0l3H2",
   },
   {
-    title: "My Resume Site",
+    title: "My linkedIn profile",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Linked in much important as it is your updated resume which show your abilities.",
+    url: "https://www.linkedin.com/in/abdul-hanan-81ba64237/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "How to use Github",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Concept of github and its uses is explained in this video.",
+    url: "https://youtu.be/PQsJR8ci3J0?si=mZufxV4F2gIsvTvZ",
   },
 ];
 
