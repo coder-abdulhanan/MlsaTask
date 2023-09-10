c3981dcc28706de602c63d7e5c4e465386956135
Abdul hanan <abdulhanan78860@gmail.com>
2023-09-10 16:19:20 +0500

    new changes

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,7 +20,7 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/nature.jpg";
+import image from "../images/sunset.jpg";
 const imageAltText = "Beatiful sea view with clouds and peacful sky color";
 
 const Home = ({ name, title }) => {
